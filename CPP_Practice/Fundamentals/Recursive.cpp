f4ad58d8874a63ba10295dee5db474a0c4f90ce1
Nathiya Xavier <nathi.pearl@gmail.com>
2021-04-26 21:31:57 +0200

    Recursive file added

---
+++ CPP_Practice/Fundamentals/Recursive.cpp
@@ -0,0 +1,28 @@
+﻿//Demo on recursive function
+#include <iostream>
+#include <cstdint>
+
+//factorial function using recursive
+// Eg: 5! = 5*4*3*2*1
+double factorial(const int32_t n) {
+
+	if (n > 1)
+	{
+		return(n * factorial(n - 1));
+	}
+	else
+		return 1;
+
+}
+
+//main program;
+int main() {
+
+	int32_t n_value;
+	std::cout << "Enter values for factorial calculation : ";
+	std::cin >> n_value;
+
+	std::cout << "factorial of " << n_value << " is : " << factorial(n_value);
+	std::cout << '\n';
+	return 0;
+}
